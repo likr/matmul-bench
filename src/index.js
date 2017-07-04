3a3593d0e4fe61015ed629a0193106f29f415087
Yosuke ONOUE <onoue@likr-lab.com>
2017-07-04 14:24:12 +0900

    Add gflops column

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
 
-const writeResult = (results) => {
+const writeResult = (results, n) => {
   const table = document.getElementById('result')
   results.forEach((result) => {
     const name = document.createElement('td')
@@ -9,9 +9,12 @@ const writeResult = (results) => {
     mean.innerHTML = (result.stats.mean * 1000).toFixed(3)
     const count = document.createElement('td')
     count.innerHTML = result.count
+    const flops = document.createElement('td')
+    flops.innerHTML = ((n * n * (2 * n + 3)) / (10 ** 9) / result.stats.mean).toFixed(3)
     const row = document.createElement('tr')
     row.appendChild(name)
     row.appendChild(mean)
+    row.appendChild(flops)
     row.appendChild(count)
     table.appendChild(row)
   })
@@ -24,6 +27,10 @@ for (const e of document.querySelectorAll('.n')) {
 
 const startButton = document.getElementById('start-button')
 startButton.addEventListener('click', () => {
+  if (!window.WebAssembly) {
+    window.alert('This benchmark requires WebAssembly support')
+    return
+  }
   startButton.disabled = true
   let cases = 0
   let finished = 0
@@ -40,7 +47,7 @@ startButton.addEventListener('click', () => {
       case 'result':
         finished += 1
         status.innerHTML = `running (${finished} / ${cases})`
-        writeResult(e.data.results)
+        writeResult(e.data.results, n)
         break
       case 'finish':
         status.innerHTML = 'done'
